5aa44f3bbccbecf5b5996479f76ad4c1eb13c914
Matthew Miner <matthew@matthewminer.com>
2014-02-01 15:55:37 -0500

    Add solution to Find Hacker Rank problem.

---
+++ findhackerrank.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Given a list of conversations, figure out which ones:
+
+- Start with "hackerrank"
+- End with "hackerrank"
+- Start and end with "hackerrank"
+
+https://www.hackerrank.com/challenges/find-hackerrank
+"""
+
+import re
+
+
+def find_conversation_ranks(conversations):
+    """Prints numeric ranks of conversations."""
+    # Compile regex patterns.
+    start_pattern = re.compile('^hackerrank')
+    end_pattern = re.compile('hackerrank$')
+    start_and_end_pattern = re.compile('^hackerrank$')
+
+    for conversation in conversations:
+        if start_and_end_pattern.search(conversation):
+            print(0)
+        elif start_pattern.search(conversation):
+            print(1)
+        elif end_pattern.search(conversation):
+            print(2)
+        else:
+            print(-1)
+
+
+if __name__ == '__main__':
+    conversation_count = int(input())
+    conversations = [input() for _ in range(conversation_count)]
+    find_conversation_ranks(conversations)
