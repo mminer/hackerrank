301b8d6ff8affe4d0d2828c1a583972597138f52
Matthew Miner <matthew@matthewminer.com>
2014-02-01 15:46:51 -0500

    Add solution to Pairs problem.

---
+++ pairs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Given N integers, count the total pairs of integers that have a difference of
+K.
+
+https://www.hackerrank.com/challenges/pairs
+"""
+
+
+def find_pairs_count(numbers, k):
+    """Finds how many pairs of numbers have a difference of k."""
+    answer = 0
+    distinct = set(numbers)
+
+    for number in numbers:
+        difference = number - k
+
+        if difference in distinct:
+            answer += 1
+
+    return answer
+
+
+def numbers_from_input():
+    """Reads a list of integers from sdtin."""
+    tokens = input().split()
+    numbers = list(map(int, tokens))
+    return numbers
+
+
+if __name__ == '__main__':
+    n, k = numbers_from_input()
+    numbers = numbers_from_input()
+    pairs_count = find_pairs_count(numbers, k)
+    print(pairs_count)
