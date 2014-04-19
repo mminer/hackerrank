90b6f794409dfa9fd1df35b6e31d40da42d3cf84
Matthew Miner <matthew@matthewminer.com>
2014-04-19 18:22:09 -0400

    Add solution to Saying Hi challenge.

---
+++ saying-hi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Determine which lines match the pattern 'hi<BLANK>' and aren't immediately
+followed by the letter D. Both 'hi' and D are case-insensitive.
+
+https://www.hackerrank.com/challenges/saying-hi
+"""
+
+import re
+
+
+def print_valid_lines(lines):
+    """Prints lines that meet the constraints."""
+    pattern = re.compile('^hi [^d].*$', re.IGNORECASE)
+
+    for line in lines:
+        if pattern.match(line):
+            print(line)
+
+
+if __name__ == '__main__':
+    line_count = int(input())
+    lines = [input() for _ in range(line_count)]
+    print_valid_lines(lines)
