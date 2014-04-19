57ba61579731ee85106e6c6885a1cd200dee9c52
Matthew Miner <matthew@matthewminer.com>
2014-04-19 16:29:26 -0400

    Improve readability of FizzBuzz solution.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 """
-Prints numbers 1 to 100. Prints "Fizz" for multiples of three. Prints "Buzz"
-for multiples of five. Prints "FizzBuzz" for multiples of both three and five.
+Prints numbers 1 to 100.
+Prints "Fizz" for multiples of three.
+Prints "Buzz" for multiples of five.
+Prints "FizzBuzz" for multiples of both three and five.
 
 https://www.hackerrank.com/challenges/fizzbuzz
 """
@@ -10,11 +12,11 @@ https://www.hackerrank.com/challenges/fizzbuzz
 
 def fizz_buzz(n=100):
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 3 and i % 5:
             print('FizzBuzz')
-        elif i % 3 == 0:
+        elif i % 3:
             print('Fizz')
-        elif i % 5 == 0:
+        elif i % 5:
             print('Buzz')
         else:
             print(i)
