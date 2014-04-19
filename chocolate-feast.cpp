0190ff6f3e9aa6d5fe7520df8b6e5885d2c94028
Matthew Miner <matthew@matthewminer.com>
2014-04-19 16:25:36 -0400

    Add solution to Chocolate Feast challenge.

---
+++ chocolate-feast.cpp
@@ -0,0 +1,29 @@
+// Determines how many chocolates of price $C can be purchased given a $N bill.
+// An additional chocolate is free for every M wrappers traded.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n, c, m;
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> c >> m;
+        int answer = n / c;
+        int wrappers = answer;
+
+        // Spend wrappers for additional chocolates.
+        while (wrappers >= m) {
+            wrappers -= m;
+
+            // Eat chocolate, produce new wrapper.
+            answer++;
+            wrappers++;
+        }
+
+        cout << answer << endl;
+    }
+
+    return 0;
+}
