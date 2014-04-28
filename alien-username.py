a5e6f8226bd403e16f08ef829decc5bdce57b6aa
Matthew Miner <matthew@matthewminer.com>
2014-04-27 20:08:42 -0400

    Add solution to Alien Username challenge.

---
+++ alien-username.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Identifies whether fictional alien usernames are valid.
+
+https://www.hackerrank.com/challenges/alien-username
+"""
+
+import re
+
+
+def validate_usernames(usernames):
+    """Prints 'VALID' for valid usernames, 'INVALID' otherwise."""
+    pattern = re.compile('^[_\.]\d+[a-z]*_?$', re.IGNORECASE)
+
+    for username in usernames:
+        print('VALID' if pattern.match(username) else 'INVALID')
+
+
+if __name__ == '__main__':
+    username_count = int(input())
+    usernames = [input() for _ in range(username_count)]
+    validate_usernames(usernames)
