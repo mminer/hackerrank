0b30948d30810f0cd2ac91a32de3d9576afc1ce1
Matthew Miner <matthew@matthewminer.com>
2014-08-17 21:15:18 -0700

    Add solution to Gem Stones challenge.

---
+++ gem-stones.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Counts the number of elements that appear in all given rocks.
+
+https://www.hackerrank.com/challenges/gem-stones
+"""
+
+from functools import reduce
+
+
+def count_gem_elements(rocks):
+    """Finds the number of 'gem elements' in the given rocks."""
+    # Remove duplicate elements by treating the elements in each rock as a set.
+    # Finding the intersection of these sets gives us elements in all rocks.
+    element_sets = (set(rock) for rock in rocks)
+    gem_elements = reduce(lambda x, y: x & y, element_sets)
+    return len(gem_elements)
+
+
+if __name__ == '__main__':
+    rock_count = int(input())
+    rocks = (input() for _ in range(rock_count))
+    print(count_gem_elements(rocks))
