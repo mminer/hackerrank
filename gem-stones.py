661c409b11939a6c8d4f56584f79c5dfc36332b8
Matthew Miner <matthew@matthewminer.com>
2014-08-18 21:25:01 -0700

    Use set's builtin intersection function.

--- gem-stones.py
+++ gem-stones.py
@@ -6,15 +6,13 @@ Counts the number of elements that appear in all given rocks.
 https://www.hackerrank.com/challenges/gem-stones
 """
 
-from functools import reduce
-
 
 def count_gem_elements(rocks):
     """Finds the number of 'gem elements' in the given rocks."""
     # Remove duplicate elements by treating the elements in each rock as a set.
     # Finding the intersection of these sets gives us elements in all rocks.
-    element_sets = (set(rock) for rock in rocks)
-    gem_elements = reduce(lambda x, y: x & y, element_sets)
+    element_sets = [set(rock) for rock in rocks]
+    gem_elements = set.intersection(*element_sets)
     return len(gem_elements)
 
 
